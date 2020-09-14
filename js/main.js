1fd3f07c79251b5ab3a5eaf89141edda1495b74e
charly95000 <charlywandja@gmail.com>
2020-09-14 22:55:28 +0200

    correctif

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 (function ($) {
 	"use strict";
 
-	$(window).on('scroll', function (){
-		if ($(this).scrollTop()> 20){
-			$('.navbar').removeClass("bg-transparent");
-			$('.navbar-toggler').removeClass("bg-transparent");
-			$('.navbar').addClass("shadow-sm");
-			$('.navbar-toggler').addClass("shadows-sm");
-		}
-		else{
-			$('.navbar').removeClass("shadow-sm");
-			$('.navbar-toggler').removeClass("shadows-sm");
-            $('.navbar').addClass("bg-transparent");
-			$('.navbar-toggler').addClass("bg-transparent");
-		}
+	
+
+	$(".nav-link").click(function () {
+		$(".navbar-collapse").collapse("hide");
 	});
 })(window.jQuery);
